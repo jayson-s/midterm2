d61eeb744190dad9329bf202d251fde708e03794
jayson-s <jayson.sandhu@ontariotechu.net>
2022-03-25 23:59:02 -0400

    1 file modified, 1 added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,25 +12,14 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
+    <a href="https://jsonplaceholder.typicode.com/users" target="_blank">Home</a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a href="https://jsonplaceholder.typicode.com/todos" target="_blank">Tasks</a>
+    |
+    <a href="https://jsonplaceholder.typicode.com/users" target="_blank">Login</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  
 </template>
 
 <style scoped>
